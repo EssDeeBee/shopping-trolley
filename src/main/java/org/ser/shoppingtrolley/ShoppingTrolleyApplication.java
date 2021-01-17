4dce0f13dc5fb7bd2bd9acf499da165046d52542
Sergey Dzeboev <sergey.dzeboev@drsmile.de>
2021-01-17 22:39:53 +0300

    Initial commit

---
+++ src/main/java/org/ser/shoppingtrolley/ShoppingTrolleyApplication.java
@@ -0,0 +1,13 @@
+package org.ser.shoppingtrolley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingTrolleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingTrolleyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ser/shoppingtrolley/ShoppingTrolleyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ser.shoppingtrolley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingTrolleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
